74e8406136f57e1efd0be6794a641423bffbee16
isai villanueva~git configssh-keygen -t rsa -b 4096 -C isai.villanu.7@gmail.comgit config --global user.name isai <isai.villanu.7@gmail.com>
2019-09-28 02:10:36 -0700

    creating app database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+  ### Populate data with 3 starter burgers
+INSERT INTO burger (burger_name) VALUES ('Double Bacon Cheeseburger');
+
+INSERT INTO burger (burger_name) VALUES ('Truffle Waffle Burger');
+
+INSERT INTO burger (burger_name) VALUES ('Blue Cheese Bison Burger');
