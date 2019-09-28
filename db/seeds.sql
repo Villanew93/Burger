a91a7aa131514a456af86970595c7e40409f686f
isai villanueva~git configssh-keygen -t rsa -b 4096 -C isai.villanu.7@gmail.comgit config --global user.name isai <isai.villanu.7@gmail.com>
2019-09-28 03:37:35 -0700

    debuging

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-  ### Populate data with 3 starter burgers
+
 INSERT INTO burger (burger_name) VALUES ('Double Bacon Cheeseburger');
 
 INSERT INTO burger (burger_name) VALUES ('Truffle Waffle Burger');
